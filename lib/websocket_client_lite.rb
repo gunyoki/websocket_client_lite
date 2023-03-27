f9cc456775d5eaa69b2046948aa8e927bb02bebd
Ueda Satoshi <gunyoki@gmail.com>
2023-03-27 15:41:18 +0900

    first commit

---
+++ lib/websocket_client_lite.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "websocket_client_lite/version"
+
+module WebsocketClientLite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/websocket_client_lite_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WebsocketClientLite do
+  it "has a version number" do
+    expect(WebsocketClientLite::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
